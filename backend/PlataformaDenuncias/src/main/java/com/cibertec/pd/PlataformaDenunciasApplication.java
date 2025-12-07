ca278262b935d8e5ea4fee294e1553447d5a5457
sebaszmAlt <sebaszm2801@gmail.com>
2025-12-06 19:51:47 -0500

    Proyecto inicial

---
+++ backend/PlataformaDenuncias/src/main/java/com/cibertec/pd/PlataformaDenunciasApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec.pd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaDenunciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaDenunciasApplication.class, args);
+	}
+
+}
